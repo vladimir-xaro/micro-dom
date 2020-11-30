3a0aae9e2fe205529742c09d4d8e71c34d9b81d0
Vladimir Slavgorodskiy <vladimir.xaro@gmail.com>
2020-11-30 15:39:06 +0300

    fixed hasClass type, added MicroDOM class to export

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,13 @@ import _ from "./entry";
 
 export default _;
 
+// ===
+
+import MicroDOM from "./MicroDOM";
+import { I_MicroDOM } from "./types";
+
 export {
+  MicroDOM,
+
   I_MicroDOM
-} from './types';
+};
